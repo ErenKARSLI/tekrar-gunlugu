8422af273f2d02107d46dd8a4db754fabfb3184d
Eren Karslı <karslieren61@gmail.com>
2025-04-28 21:30:10 +0300

    form-validation

---
+++ 13-form-validation/form.js
@@ -0,0 +1,7 @@
+const form  = document.getElementById("kullaniciFormu");
+const hata  = document.getElementById("hataMesaji");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  // alanları oku ve validate et...
+});
